79c532ded7700ffa03071f2d714d0620bd3fe04e
Paul Francis <paul@francis.com>
2020-11-18 16:58:30 +0100

    rowFiller.py now running a basic attack scenario

--- rowFiller.py
+++ rowFiller.py
@@ -2,34 +2,87 @@ import sqlite3
 import pprint
 import os.path
 import itertools
+import pandas as pd
+import numpy as np
 import whereParser
 
 class rowFiller:
     """Generates the rows as sqlite commands
     """
-    def __init__(self, sw, pruneSql,
+    def __init__(self, sw,
             numAids=1,
             aidDist='distinctPerRow',
             numRowsPerCombination=10):
         self.sw = sw
-        self.pruneSql = pruneSql
         self.numAids = numAids
         self.aidDist = aidDist
         self.numRowsPerCombination = numRowsPerCombination
         self.maxDbName = 50
         self.dbName = self._makeDbName()
         self.dbPath = os.path.join('tables',self.dbName)
+        self.failedCombinations = []
+        # This will be one list per table
+        self.baseData = {}
+        self.baseDf = {}
+
+    def queryDb(self,sql):
         self.conn = sqlite3.connect(self.dbPath)
         self.cur = self.conn.cursor()
-        self.failedCombinations = []
+        self.cur.execute(sql)
+        answer = self.cur.fetchall()
+        self.conn.close()
+        return answer
+
+    def queryDf(self,table,query):
+        df = self.baseDf[table].query(query)
+        return df
+
+    def makeBaseTables(self):
+        ''' This builds the basic table that has as many matching combinations
+            as possible.
+        '''
         for table in self.sw.iterTabs():
+            self.baseData[table] = []
             self.conditions = list(self.sw.iterConditions(table))
-            self._processOneTable(table)
+            self._processOneTable(table,self.baseData[table])
+    
+    def baseTablesToDb(self):
+        ''' This takes the base table and writes it to an sql db '''
+        for table,data in self.baseData.items():
+            allColumns = []
+            for i in range(self.numAids):
+                allColumns.append(f"aid{i+1}")
+            for column in list(self.sw.iterCols(table)):
+                allColumns.append(column)
+            self.baseDf[table] = pd.DataFrame(data, columns=allColumns) 
+        self.conn = sqlite3.connect(self.dbPath)
+        for table,df in self.baseDf.items():
+            df.to_sql(table,con=self.conn, if_exists='replace')
+        self.conn.close()
+
+    def pruneBaseTables(self,table,query,numLeft=1):
+        ''' This prunes away the rows that match the dataframe query leaving numLeft
+            number of distinct AIDs
+        '''
+        bdf = self.baseDf[table]
+        dfRemove = bdf.query(query)
+        notQuery = f"not({query})"
+        dfKeep = bdf.query(notQuery)
+        # dfRemove contains the rows that we want to drop
+        # dfKeep contains everything else
+        # We want to shift rows for numLeft distinct users from dfRemove to dfKeep
+        for _ in range(numLeft):
+            aidVal = dfRemove['aid1'].iloc[0]
+            dfShift = dfRemove.query("aid1 == @aidVal")
+            dfKeep = dfKeep.append(dfShift)
+            dfRemove = dfRemove.query("aid1 != @aidVal")
+        self.baseDf[table] = dfKeep
+        self.conn = sqlite3.connect(self.dbPath)
+        dfKeep.to_sql(table,con=self.conn, if_exists='replace')
         self.conn.close()
     
-    def _processOneTable(self,table):
+    def _processOneTable(self,table,data):
         columns = list(self.sw.iterCols(table))
-        self._defineTableInDb(table,columns)
         # For now we have one distinct AID per row, numerically increasing
         # Later we'll have different AID distributions and multiple AIDs
         aids = [1]
@@ -75,9 +128,8 @@ class rowFiller:
             # `values` contains the list of working values in the order that the columns
             # appear in the sqlite table
             for _ in range(self.numRowsPerCombination):
-                self._makeRow(table,aids,values)
+                self._makeRow(data,aids,values)
                 aids[0] += 1
-        self.conn.commit()
 
     def _addFailedCombination(self,columns,column,comb,conditions,values):
         self.failedCombinations.append({'columns':columns,
@@ -114,18 +166,13 @@ class rowFiller:
                 continue    # try next value
         return None
 
-    def _makeRow(self,table,aids,values):
-        sql = f'''INSERT INTO {table} VALUES ('''
+    def _makeRow(self,data,aids,values):
+        row = []
         for aid in aids:
-            sql += f'''{aid}, '''
+            row.append(aid)
         for value in values:
-            if type(value) is str:
-                sql += f"'{value}', "
-            else:
-                sql += f"{value}, "
-        sql = sql[:-2]
-        sql += ')'
-        self.cur.execute(sql)
+            row.append(value)
+        data.append(row)
 
     def _addCandidateValues(self,candidateValues,condition,result):
         operation = self.sw.getOperation(condition)
@@ -188,29 +235,6 @@ class rowFiller:
             print(f"Error: valuePasses: no matching branch {condition}, {result}")
             quit()
 
-    def _getValueFromCondition(self,condition,result):
-        column = self.sw.getColName(condition)
-        operation = self.sw.getOperation(condition)
-        operands = self.sw.getOperands(condition)
-        if ((operation == 'eq' and result is True) or
-            (operation == 'neq' and result is False) or
-            (operation == 'between' and result is True)):
-            value = operands[0]
-        elif ((operation == 'eq' and result is False) or
-            (operation == 'neq' and result is True) or
-            ((operation == 'gt' or operation == 'gte') and result is True) or
-            ((operation == 'lt' or operation == 'lte') and result is False)):
-            value = self._makeBiggerValue(operands[0])
-        elif (((operation == 'gt' or operation == 'gte') and result is False) or
-            ((operation == 'lt' or operation == 'lte') and result is True)):
-            value = self._makeSmallerValue(operands[0])
-        elif (operation == 'between' and result is False):
-            value = self._makeBiggerValue(operands[1])
-        else:
-            print(f"Error: getValueFromCondition: no matching branch {condition}, {result}")
-            quit()
-        return column,value
-
     def _addBiggerValues(self,value,valList):
         if type(value) is str:
             # Not guaranteed to be bigger, but good chance
@@ -241,21 +265,6 @@ class rowFiller:
         else:
             return value-1
 
-    def _defineTableInDb(self,table,columns):
-        sql = f'''DROP TABLE IF EXISTS {table}'''
-        self.cur.execute(sql)
-        sql = f'''CREATE TABLE {table} ('''
-        # add AID columns
-        for i in range(self.numAids):
-            sql += f'''aid{i} integer, '''
-        # add columns
-        for column in columns:
-            sql += f'''{column} {self.sw.getColType(table,column)}, '''
-        # strip last comma-space
-        sql = sql[:-2]
-        sql += ')'
-        self.cur.execute(sql)
-
     def _makeDbName(self):
         dbName = ''
         for table in self.sw.iterTabs():
@@ -272,13 +281,32 @@ class rowFiller:
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
     tests = [
-        {   'sql': "select count(*) from tab where t1='y' or i1=1 or r1=1.1",
-            'prune': "select * from tab where (t1='y' or i1=1) and r1=1.1 ",
+        {   # The attack here is where there is one user with i1=12345. We want to know
+            # if that user has value t1='y' or not.
+            'sql': "select count(*) from tab where t1='y' or i1=12345",
+            'attack1': "select count(*) from tab where t1='y' or i1=12345",
+            'attack2': "select count(*) from tab where t1='y'",
+            # I want to make a scenario where the victim does not have t1=y. So I prune all
+            # but one of the users that has i1=12345 but not t1=y
+            'prune': {'table':'tab','query': "t1 != 'y' and i1 == 12345"},
+            'ansPrune': 10
         },
     ]
     for test in tests:
         sw = whereParser.simpleWhere(test['sql'])
-        rf = rowFiller(sw,test['prune'])
+        rf = rowFiller(sw)
+        rf.makeBaseTables()
+        if len(rf.failedCombinations) > 0:
+            print("Failed Combinations:")
+            pp.pprint(rf.failedCombinations)
+        rf.baseTablesToDb()
+        print("Original base dataframe:")
+        pp.pprint(rf.baseDf)
         print(rf.dbPath)
-        print(test['sql'])
-        pp.pprint(rf.failedCombinations)
+        rf.pruneBaseTables(test['prune']['table'],test['prune']['query'])
+        print("Pruned base dataframe:")
+        pp.pprint(rf.baseDf)
+        print(f"{test['attack1']}:")
+        pp.pprint(rf.queryDb(test['attack1']))
+        print(f"{test['attack2']}:")
+        pp.pprint(rf.queryDb(test['attack2']))
